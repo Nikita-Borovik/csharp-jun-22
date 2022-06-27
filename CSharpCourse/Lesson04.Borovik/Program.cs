05038b58d7bf6a415b3d8e959f5a3759083729e1
Никита Боровик <nikita.asipov@gmail.com>
2022-06-27 19:54:13 +0300

    Add Nikita Borovik Project

--- CSharpCourse/Lesson04.Borovik/Program.cs
+++ CSharpCourse/Lesson04.Borovik/Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello there!");
-Console.WriteLine(DateTime.Now);
+Console.WriteLine("Hey there!");
+DateTime now = DateTime.Now;
+Console.WriteLine($"Now is: {now}");
+
